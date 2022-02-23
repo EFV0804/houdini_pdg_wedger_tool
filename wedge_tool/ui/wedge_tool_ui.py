b13bb6464be5a89a778c891b20b91d8febcc0e59
Elise Vidal <evidal@artfx.fr>
2022-02-23 15:00:43 +0100

    clean work folder, delete unnecessary files

--- wedge_tool/ui/wedge_tool_ui.py
+++
@@ -1,7 +0,0 @@
-from Qt import QtWidgets, QtCompat, QtCore
-import wedge_tool
-class OpenFileWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(OpenFileWindow, self).__init__()
-        self.wedger = wedge_tool.Wedger()
-        self.data = wedger.user_settings
